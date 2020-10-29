61e9834ff8e04babe511bbaff57fb037938b6764
Airels <yohan13240@hotmail.fr>
2020-10-29 18:02:31 +0100

    Correction affichage portes + réimplémentation affichage chemins

--- src/main/java/view/GUIRoom.java
+++ src/main/java/view/GUIRoom.java
@@ -26,16 +26,16 @@ public class GUIRoom implements GUIObject {
         Set<Direction> waysWithDoor = room.getDoorWays();
 
         if (openedWays.contains(Direction.LEFT) || waysWithDoor.contains(Direction.LEFT)) {
-            rectangleBuffer = new Rectangle(0, 0, App.WALL_SIZE, App.HEIGHT/14*5);
+            rectangleBuffer = new Rectangle(0, 0, App.WALL_SIZE, App.HEIGHT*0.40);
             rectangleBuffer.setFill(App.WALL_COLOR);
             rectangles.add(rectangleBuffer);
 
-            rectangleBuffer = new Rectangle(0, App.HEIGHT/14*9, App.WALL_SIZE, App.HEIGHT/10*5);
+            rectangleBuffer = new Rectangle(0, App.HEIGHT*0.60, App.WALL_SIZE, App.HEIGHT*0.40);
             rectangleBuffer.setFill(App.WALL_COLOR);
             rectangles.add(rectangleBuffer);
 
             if (waysWithDoor.contains(Direction.LEFT)) {
-                rectangleBuffer = new Rectangle(0, App.HEIGHT/14*5, App.WALL_SIZE, App.HEIGHT/14*9);
+                rectangleBuffer = new Rectangle(0, App.HEIGHT*0.40, App.WALL_SIZE, App.HEIGHT*0.20);
                 rectangleBuffer.setFill(App.DOOR_COLOR);
                 rectangles.add(rectangleBuffer);
             }
@@ -46,16 +46,16 @@ public class GUIRoom implements GUIObject {
         }
 
         if (openedWays.contains(Direction.UP) || waysWithDoor.contains(Direction.UP)) {
-            rectangleBuffer = new Rectangle(0, 0, App.WIDTH/14*5, App.WALL_SIZE);
+            rectangleBuffer = new Rectangle(0, 0, App.WIDTH*0.40, App.WALL_SIZE);
             rectangleBuffer.setFill(App.WALL_COLOR);
             rectangles.add(rectangleBuffer);
 
-            rectangleBuffer = new Rectangle(App.WIDTH/14*9, 0, App.WIDTH/14*5, App.WALL_SIZE);
+            rectangleBuffer = new Rectangle(App.WIDTH*0.60, 0, App.WIDTH*0.40, App.WALL_SIZE);
             rectangleBuffer.setFill(App.WALL_COLOR);
             rectangles.add(rectangleBuffer);
 
             if (waysWithDoor.contains(Direction.UP)) {
-                rectangleBuffer = new Rectangle(App.HEIGHT/14*5, 0, App.HEIGHT/14*9, App.WALL_SIZE);
+                rectangleBuffer = new Rectangle(App.WIDTH*0.40, 0, App.WIDTH*0.20, App.WALL_SIZE);
                 rectangleBuffer.setFill(App.DOOR_COLOR);
                 rectangles.add(rectangleBuffer);
             }
@@ -66,16 +66,16 @@ public class GUIRoom implements GUIObject {
         }
 
         if (openedWays.contains(Direction.RIGHT) || waysWithDoor.contains(Direction.RIGHT)) {
-            rectangleBuffer = new Rectangle(App.WIDTH-App.WALL_SIZE, 0, App.WALL_SIZE, App.HEIGHT/14*5);
+            rectangleBuffer = new Rectangle(App.WIDTH-App.WALL_SIZE, 0, App.WALL_SIZE, App.HEIGHT*0.40);
             rectangleBuffer.setFill(App.WALL_COLOR);
             rectangles.add(rectangleBuffer);
 
-            rectangleBuffer = new Rectangle(App.WIDTH-App.WALL_SIZE, App.HEIGHT/14*9, App.WALL_SIZE, App.HEIGHT/14*5);
+            rectangleBuffer = new Rectangle(App.WIDTH-App.WALL_SIZE, App.HEIGHT*0.60, App.WALL_SIZE, App.HEIGHT*0.40);
             rectangleBuffer.setFill(App.WALL_COLOR);
             rectangles.add(rectangleBuffer);
 
             if (waysWithDoor.contains(Direction.RIGHT)) {
-                rectangleBuffer = new Rectangle(App.WIDTH-App.WALL_SIZE, App.HEIGHT/14*5, App.WALL_SIZE, App.HEIGHT/14*9);
+                rectangleBuffer = new Rectangle(App.WIDTH-App.WALL_SIZE, App.HEIGHT*0.40, App.WALL_SIZE, App.HEIGHT*0.20);
                 rectangleBuffer.setFill(App.DOOR_COLOR);
                 rectangles.add(rectangleBuffer);
             }
@@ -86,16 +86,16 @@ public class GUIRoom implements GUIObject {
         }
 
         if (openedWays.contains(Direction.DOWN)) {
-            rectangleBuffer = new Rectangle(0, App.HEIGHT-App.WALL_SIZE, App.WIDTH/14*5, App.WALL_SIZE);
+            rectangleBuffer = new Rectangle(0, App.HEIGHT-App.WALL_SIZE, App.WIDTH*0.40, App.WALL_SIZE);
             rectangleBuffer.setFill(App.WALL_COLOR);
             rectangles.add(rectangleBuffer);
 
-            rectangleBuffer = new Rectangle(App.WIDTH/14*9, App.HEIGHT-App.WALL_SIZE, App.WIDTH/14*5, App.WALL_SIZE);
+            rectangleBuffer = new Rectangle(App.WIDTH*0.60, App.HEIGHT-App.WALL_SIZE, App.WIDTH*0.40, App.WALL_SIZE);
             rectangleBuffer.setFill(App.WALL_COLOR);
             rectangles.add(rectangleBuffer);
 
             if (waysWithDoor.contains(Direction.DOWN)) {
-                rectangleBuffer = new Rectangle(App.HEIGHT/14*5, App.HEIGHT-App.WALL_SIZE, App.HEIGHT/14*9, App.WALL_SIZE);
+                rectangleBuffer = new Rectangle(App.WIDTH*0.40, App.HEIGHT-App.WALL_SIZE, App.WIDTH*0.20, App.WALL_SIZE);
                 rectangleBuffer.setFill(App.DOOR_COLOR);
                 rectangles.add(rectangleBuffer);
             }
