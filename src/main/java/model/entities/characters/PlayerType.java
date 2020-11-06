7ede24f0e58c7521d737599aeefef33c73a4ec4f
Airels <yohan13240@hotmail.fr>
2020-11-06 12:32:08 +0100

    Modification couleur 2eme personnage

--- src/main/java/model/entities/characters/PlayerType.java
+++ src/main/java/model/entities/characters/PlayerType.java
@@ -5,7 +5,7 @@ import javafx.scene.paint.Color;
 public enum PlayerType {
 
     NORMAL(100, 20, 40, 1, 20, Color.GREEN),
-    GOD_OF_HYPERDEATH(1_000_000, 1_000_000, 40, 1, 20, Color.PURPLE);
+    GOD_OF_HYPERDEATH(1_000_000, 1_000_000, 40, 1, 20, Color.LIGHTGREEN);
 
     PlayerType(int healthPoints, int strength, int actionRange, double speed, double size, Color color) {
         this.healthPoints = healthPoints;
