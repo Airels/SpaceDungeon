687d734cbac9126768489b8aa86499d13e408b16
Airels <yohan13240@hotmail.fr>
2020-11-11 14:36:57 +0100

    Assignation des touches pour l'inventaire

--- src/main/java/controller/JavaFXController.java
+++ src/main/java/controller/JavaFXController.java
@@ -4,18 +4,60 @@ import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import model.Direction;
+import model.Game;
+import model.entities.characters.Inventory;
 import model.entities.characters.players.Player;
+import model.items.Item;
 
 public class JavaFXController {
 
     public static EventHandler<? super KeyEvent> getKeyEventHandler(Player player) {
         return (EventHandler<KeyEvent>) event -> {
+            Item item;
+
             switch (event.getCode()) {
                 case Z: case W: case UP:    player.move(Direction.UP);    break;
                 case Q: case A: case LEFT:  player.move(Direction.LEFT);  break;
                 case S:         case DOWN:  player.move(Direction.DOWN);  break;
                 case D:         case RIGHT: player.move(Direction.RIGHT); break;
                 case E: player.action(); break;
+
+                case DIGIT1:
+                    item = player.getInventory().getItem(0);
+                    if (item != null) item.action();
+                    break;
+                case DIGIT2:
+                    item = player.getInventory().getItem(1);
+                    if (item != null) item.action();
+                    break;
+                case DIGIT3:
+                    item = player.getInventory().getItem(2);
+                    if (item != null) item.action();
+                    break;
+                case DIGIT4:
+                    item = player.getInventory().getItem(3);
+                    if (item != null) item.action();
+                    break;
+                case DIGIT5:
+                    item = player.getInventory().getItem(4);
+                    if (item != null) item.action();
+                    break;
+                case DIGIT6:
+                    item = player.getInventory().getItem(5);
+                    if (item != null) item.action();
+                    break;
+                case DIGIT7:
+                    item = player.getInventory().getItem(6);
+                    if (item != null) item.action();
+                    break;
+                case DIGIT8:
+                    item = player.getInventory().getItem(7);
+                    if (item != null) item.action();
+                    break;
+                case DIGIT9:
+                    item = player.getInventory().getItem(8);
+                    if (item != null) item.action();
+                    break;
             }
         };
     }
