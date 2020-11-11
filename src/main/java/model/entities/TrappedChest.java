3da8444105223c3e89e5e5c3eaf4b2936037aeb1
GrosFlan <guillaume13.nicolai@gmail.com>
2020-11-11 17:07:00 +0100

    Modification de la classe TrappedChest pour faire apparaitre un swarm dans la salle

--- src/main/java/model/entities/TrappedChest.java
+++ src/main/java/model/entities/TrappedChest.java
@@ -1,14 +1,19 @@
 package model.entities;
 
+import model.Coordinates;
+import model.Game;
 import model.entities.characters.monsters.Monster;
 import model.entities.characters.monsters.MonsterType;
+import model.entities.characters.players.Player;
+import model.rooms.Room;
 
 
 public class TrappedChest extends Entity{
     private MonsterType monster = MonsterType.SWARM;
 
     public void action(){
-        new Monster(monster).spawn();
+        Game game = Game.getInstance();
+        game.getActualRoom().getEntities().add(new Monster(MonsterType.SWARM));
     }
 
 }
