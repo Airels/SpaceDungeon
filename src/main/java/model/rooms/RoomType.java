a2427efd213a4cae1cab72018c2cc7b84871127e
Airels <yohan13240@hotmail.fr>
2020-11-13 13:55:55 +0100

    Ajout méthode salle aléatoire

--- src/main/java/model/rooms/RoomType.java
+++ src/main/java/model/rooms/RoomType.java
@@ -1,7 +1,13 @@
 package model.rooms;
 
+import model.generators.LabyrinthGenerator;
+
 public enum RoomType {
     SIMPLE_ROOM,
     MONSTER_ROOM,
     BOSS_ROOM;
+
+    public static RoomType getRandomRoomType() {
+        return (Math.random()*2 < 1) ? SIMPLE_ROOM : MONSTER_ROOM;
+    }
 }
