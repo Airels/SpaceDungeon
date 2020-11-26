c90f6aa8025281e6d569c5e5d5e849f6bc825ce7
Airels <yohan13240@hotmail.fr>
2020-11-26 11:25:13 +0100

    Implémentation AssetRoom

--- src/main/java/view/assetsLoader/rooms/AssetRoom.java
+++ src/main/java/view/assetsLoader/rooms/AssetRoom.java
@@ -1,5 +1,6 @@
 package view.assetsLoader.rooms;
 
+import exceptions.MissingAssetException;
 import javafx.scene.image.Image;
 import model.rooms.RoomType;
 import view.assetsLoader.AssetLoaderHelper;
@@ -7,6 +8,15 @@ import view.assetsLoader.AssetLoaderHelper;
 public class AssetRoom implements AssetLoaderHelper<RoomType> {
     @Override
     public Image load(RoomType roomType) {
-        return null;
+        switch (roomType) {
+            case SIMPLE_ROOM:
+                return new AssetSimpleRoom().load();
+            case MONSTER_ROOM:
+                return new AssetMonsterRoom().load();
+            case BOSS_ROOM:
+                return new AssetBossRoom().load();
+        }
+
+        throw new MissingAssetException();
     }
 }
